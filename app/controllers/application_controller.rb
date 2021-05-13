390223d13db6b17aca511651b362e5c5820e64d3
no-ru-kk <kk201157@gmail.com>
2021-05-13 20:45:55 +0900

    ユーザー管理機能を実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
 
   private
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :lname_reading, :fname_reading, :birth_date])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :item, only:[:edit, :index, :new, :show]
+  resources :items, only:[:edit, :index, :new, :show]
   root to: "items#index"
 end
