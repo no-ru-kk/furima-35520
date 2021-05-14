effacd922a576116a7de099a228182efebd49adc
no-ru-kk <kk201157@gmail.com>
2021-05-14 11:11:10 +0900

    モデルの単体テストコードを生成
    userモデルのバリデーションの修正も行なった。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,      presence: true
-  validates :last_name,     presence: true
-  validates :first_name,    presence: true
-  validates :lname_reading, presence: true
-  validates :fname_reading, presence: true
   validates :birth_date,    presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input both half-width alphanumeric characters with 6 characters or more."}
+
+  with_options presence: true do
+    validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :lname_reading, :fname_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
 end
