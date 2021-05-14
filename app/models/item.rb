68be112c76a7c2a4f89001b408d8c99b684d0c26
no-ru-kk <kk201157@gmail.com>
2021-05-14 18:16:06 +0900

    active_storage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   # has_one :record
+  has_one_attached :image
 
   validates :category_id, :status_id, :fee_id, :prefecture_id, :days_id, numericality: { other_than: 1 } 
 
   with_options presence: true do
     validates :name, :item_explain, :price
   end
+
+  # def was_attached?
+  #   self.image.attached?
+  # end
 end
