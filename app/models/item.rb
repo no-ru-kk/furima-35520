54db402ca7eb54b0da00fc58d8d58445d9007d46
no-ru-kk <kk201157@gmail.com>
2021-05-15 09:32:56 +0900

    商品出品機能を実装
    JavaScriptを除いて。

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :category_id, :status_id, :fee_id, :prefecture_id, :days_id, numericality: { other_than: 1 } 
-
+ 
   with_options presence: true do
-    validates :name, :item_explain, :price
+    validates :name, :item_explain, :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: {with: /\A[0-9]+\z/i, message: "is invalid. Input half-width number."}
+  
   end
 
   # def was_attached?
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only:[:edit, :index, :new, :show]
+  resources :items, only:[:edit, :index, :new, :create, :show]
 end
