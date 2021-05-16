1f58081166e396facd931b9db1c74fcabe9b53ec
no-ru-kk <kk201157@gmail.com>
2021-05-16 14:52:00 +0900

    修正1
    メソッドのまとめ（before_action等）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item,           only: [:show, :edit, :update]
+  before_action :judge_user,         only: [:edit, :update]
 
   def index
     @item = Item.includes(:user).order("created_at DESC")
@@ -19,18 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_info)
       redirect_to item_path(@item.id)
     else
@@ -41,5 +37,15 @@ class ItemsController < ApplicationController
   private
   def item_info
     params.require(:item).permit(:name, :item_explain, :category_id, :status_id, :fee_id, :prefecture_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def judge_user
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
